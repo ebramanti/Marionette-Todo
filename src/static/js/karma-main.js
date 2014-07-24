864cbc10bced24a204a40b3f555b2691269aa2c9
Edward Bramanti <edward@bramanti.org>
2014-07-24 15:28:20 -0700

    Karma main update.

--- src/static/js/karma-main.js
+++ src/static/js/karma-main.js
@@ -5,6 +5,8 @@ var tests = [
 
     // ---------------------------
     // Load Specs (AKA tests)
+    'tests/spec-app-task',
+    'tests/spec-app-tasks',
     'tests/spec-app-header-view',
     'tests/spec-app-task-view',
     'tests/spec-app-list-view',
